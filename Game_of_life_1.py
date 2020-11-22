cf7029fc49c3479a7316929bba6c075d6c8a820b
Oleg20502 <oleg20502@gmail.com>
2020-11-22 23:51:16 +0300

    Removed some bugs and added pygame visualisation

--- Game_of_life_1.py
+++ Game_of_life_1.py
@@ -3,31 +3,77 @@ import numpy as np
 import pygame as pg
 from pygame.draw import *
 
+WHITE = (255, 255, 255)
+RED = (225, 0, 50)
+BLACK = (0 , 0 , 0) 
+lightBLACK = (3,3,3)
+lightGreen = (0, 255, 0)
+BLUE  = (0,0,255, 20)
+GRAY = (125, 125, 125)
+lightBlue = (64, 128, 255)
+GREEN = (0, 200, 64, 20)
+YELLOW = (225, 225, 0, 2)
+PINK = (230, 50, 230, 0)
+brown = (105, 82, 62, 255)
+darkorange3 = (205, 102, 0, 255)
+rosybrown5 = (159, 125, 125, 255)
+sandybrown = (244, 164, 96, 255)
+CLEAR = (0,0,0,0)
+
 
 class Game_of_life():
     def __init__(self, screen_width, screen_height, fps):
         self.screen_x = screen_width
         self.screen_y = screen_height
         self.fps = fps
-        self.field_height = 1000
-        self.field_width = 2000
+        self.x_bias = 5
+        self.y_bias = 5
         
     def update_generation(self):
         # Подсчет соседей для каждой клетки кроме граничных
+        m, n = self.cell_field.shape
         N = (self.cell_field[0:-2,0:-2] + self.cell_field[0:-2,1:-1] + self.cell_field[0:-2,2:] + self.cell_field[1:-1,0:-2] 
              + self.cell_field[1:-1,2:] + self.cell_field[2: ,0:-2] + self.cell_field[2: ,1:-1] + self.cell_field[2: ,2:])
         # Применение правил
         birth = (N == 3) & (self.cell_field[1:-1,1:-1] == 0)
         survive = ((N == 2) | (N == 3)) & (self.cell_field[1:-1,1:-1] == 1)
-        self.cell_field[...] = 0
-        self.cell_field[1:-1,1:-1][birth | survive] = 1
-    
+        self.new_cell_field = np.zeros((m, n))
+        self.new_cell_field[1:-1,1:-1][birth | survive] = 1
+        self.cell_field, self.old_cell_field = self.new_cell_field, self.cell_field
+        print(self.cell_field.shape)
+        
     def create_random_life(self):
-        self.cell_field = np.zeros((self.screen_height, self.screen_width))
-        self.cell_field[1:-1,1:-1] = np.random.randint(0, 2, (self.screen_height-2, self.screen_width-2))
+        self.cell_field = np.zeros((self.field_height, self.field_width))
+        self.cell_field[1:-1,1:-1] = np.random.randint(0, 2, (self.field_height-2, self.field_width-2))
         pass
     
-    def setup(self):
+    def setup(self, regime):
+        if regime == 1:
+            self.field_height = 100
+            self.field_width = 200
+            self.create_random_life()
+        else:
+            self.cell_field = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                                        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0], 
+                                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+            self.field_height, self.field_width = np.shape(self.cell_field)
+        self.update_scale()
+        pass
+    
+    def run(self):
+        self.update_scale()
+        self.update_generation()
+        self.broaden_field()
         
         pass
     
@@ -38,13 +84,13 @@ class Game_of_life():
             self.cell_field = f
         
         def handle_events(self):
-        
+        # FIXME
             pass
     
     def update_scale(self):
-        self.scale = int(min(self.screen_width / (1.0001 * self.field_width),
-                             self.screen_height / (1.0001 * self.field_height)))
-        pass
+        self.scale = int(min(self.screen_x / (1.0001 * self.field_width),
+                             self.screen_y / (1.0001 * self.field_height)))
+    
     
     def coordinates_transform(self):
         #FIXME
@@ -58,14 +104,16 @@ class Game_of_life():
         self.screen_coord = self.scale * self.index_coord
         pass
     
-    def rect_coor(self):
+    def rect_coordinetes(self):
         # Создание координат для каждой вершины кадждого квадратика
-        indeses = self.cell_field.nonzero().T
+        indeses = np.asarray(self.cell_field.nonzero()).T[:,::-1]
         self.index_coord = indeses
-        index_rect = np.hstack((indeses, indeses[:,0]+1, indeses[:,1], indeses+1, 
-                                 indeses[:,0], indeses[:,1]+1))
-        index_rect[::2] += self.x_bias
-        index_rect[1::2] += self.y_bias
+        m, n = indeses.shape
+        index_rect = np.hstack((indeses, indeses[:,0].reshape((m,1))+1, 
+                                indeses[:,1].reshape((m,1)), indeses+1, 
+                                 indeses[:,0].reshape((m,1)), indeses[:,1].reshape((m,1))+1))
+        index_rect[:,::2] += self.x_bias
+        index_rect[:,1::2] += self.y_bias
         self.screen_rect = self.scale * index_rect
         return self.screen_rect
 
@@ -92,11 +140,36 @@ class Game_of_life():
 #     VUSUALISATION
 ###################
 
-
+def draw(Rect, color, space):
+    # Рисуем квадратик,соответствующий каждой клетке
+    for r in Rect:
+        polygon(space, color, [r[0:2], r[2:4], r[4:6], r[6:8]])
         
 X, Y = 1000, 550
-FPS = 15
+FPS = 5
 
 if __name__ == '__main__':
     game = Game_of_life(X, Y, FPS)
-    game.setup()
+    game.setup(1)
+    
+    pg.init()
+    screen = pg.display.set_mode((X , Y))
+    clock = pg.time.Clock()  
+    screen.fill(WHITE)
+    
+    pg.display.update()
+    clock = pg.time.Clock()
+    finished = False
+    generation = 1
+    while not finished:
+        clock.tick(FPS)
+        game.run()
+        generation += 1
+        print('Поколение:', generation)
+        draw(game.rect_coordinetes(), (225, 0, 50), screen)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                finished = True
+        pg.display.update()
+        screen.fill(WHITE)
+    pg.quit()
