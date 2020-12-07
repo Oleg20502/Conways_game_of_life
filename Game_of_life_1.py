cb75966b671c8780c1f16613118d0fb8e7d0213a
Oleg20502 <oleg20502@gmail.com>
2020-12-08 01:08:31 +0300

    Improved coordinates and made smooth zoom

--- Game_of_life_1.py
+++ Game_of_life_1.py
@@ -14,8 +14,8 @@
 import numpy as np
 import pygame as pg
 import time as t
-from Format_transform import load_and_transform
 from pygame.draw import polygon
+from Format_transform import load_and_transform
 
 WHITE = (255, 255, 255)
 RED = (225, 0, 50)
@@ -48,8 +48,6 @@ class Game_of_life():
         self.y_index_bias = 0
         self.x_screen_bias = 0
         self.y_screen_bias = 0
-        self.x_index_coord = 0
-        self.y_index_coord = 0
         
         self.generation = 0
         
@@ -68,13 +66,13 @@ class Game_of_life():
         c, d = np.sum(self.cell_field[:, border]), np.sum(self.cell_field[:, -1-border])
         if a > 0:
             self.cell_field = np.vstack([np.zeros((1, self.field_width)), self.cell_field])
-            self.y_index_bias -= 1
+            self.y_index_bias += 1
             self.field_height += 1
         if b > 0:
             self.cell_field = np.vstack([self.cell_field, np.zeros((1, self.field_width))])
             self.field_height += 1
         if c > 0:
-            self.x_index_bias -= 1
+            self.x_index_bias += 1
             self.cell_field = np.hstack([np.zeros((self.field_height, 1)), self.cell_field])
             self.field_width += 1
         if d > 0:
@@ -84,7 +82,7 @@ class Game_of_life():
     def create_random_life(self):
         self.cell_field = np.zeros((self.field_height, self.field_width))
         self.cell_field[1:-1,1:-1] = np.random.randint(0, 2, (self.field_height-2, self.field_width-2))
-        pass
+    
     
     def setup(self, regime):
         if regime == 1:                 # Случайная жизнь
@@ -107,15 +105,14 @@ class Game_of_life():
             self.cell_field = np.zeros((self.field_height, self.field_width))
         
         self.set_scale()
-        self.get_field_shape()
-        pass
+    
     
     def run(self, run):
         if run:
             self.generation += 1
             self.broaden_field()
             self.update_generation()
-        pass
+    
     
     def load_life(self):
         Path = 'Patterns/Gosper_Gun.txt'        #FIXME
@@ -123,7 +120,6 @@ class Game_of_life():
         with open(Path, 'r') as f:
             for line in f:
                 data_list.append(line.split(','))
-                print(data_list[-1])
             self.cell_field = np.asarray(data_list, dtype='int')
         self.broaden_field(0)
         
@@ -132,53 +128,36 @@ class Game_of_life():
         self.cells = load_and_transform(Path)
     
     def set_scale(self):
-        # FIXME
-        self.scale = round(min(self.screen_x / self.field_width,
-                             self.screen_y / self.field_height))
-    
-    
-    def coordinates_transform(self):
-        #FIXME
-        # Переводим индексы клеток в массиве в координаты для screen pygame-а
-        m, n = self.index_coord.shape
-        x_bias_array = np.full(m, self.x_index_bias).reshape((m, 1))
-        y_bias_array = np.full(m, self.y_index_bias).reshape((m, 1))
-        self.screen_coord = self.scale * (self.index_coord + np.hstack([x_bias_array, y_bias_array]))
-        self.index_coord[::2] += self.x_index_bias
-        self.index_coord[1::2] += self.y_index_bias
-        self.screen_coord = self.scale * self.index_coord
-        pass
+        self.scale = np.round(np.min([self.screen_x / self.field_width,
+                             self.screen_y / self.field_height]), 2)
     
     def rect_coordinetes(self):
         # Создание координат для каждой вершины кадждого квадратика
+        #n1, m1 = self.get_mouse_index_coord(0, 0)
+        #n2, m2 = self.get_mouse_index_coord(self.screen_x, self.screen_y)
+        #indeses = np.asarray(self.cell_field[m1:m2,n1:n2].nonzero()).T[:,::-1]
         indeses = np.asarray(self.cell_field.nonzero()).T[:,::-1]
-        self.index_coord = indeses
         m, n = indeses.shape
-        index_rect = np.hstack((indeses, indeses[:,0].reshape((m,1))+1, 
+        rect = np.hstack((indeses, indeses[:,0].reshape((m,1))+1, 
                                 indeses[:,1].reshape((m,1)), indeses+1, 
                                  indeses[:,0].reshape((m,1)), indeses[:,1].reshape((m,1))+1))
-        index_rect[:, ::2] += self.x_index_bias
-        index_rect[:, 1::2] += self.y_index_bias
-        self.screen_rect = np.zeros(np.shape(index_rect))
-        self.screen_rect[:, ::2] = self.scale * (index_rect[:, ::2]-self.x_index_coord) + self.x_screen_bias
-        self.screen_rect[:, 1::2] = self.scale * (index_rect[:, 1::2]-self.y_index_coord) + self.y_screen_bias
-        return self.screen_rect
-    
-    def get_field_shape(self):
-        self.m, self.n = np.shape(self.cell_field)
+        rect[:, ::2] = self.scale * (rect[:, ::2] - self.x_index_bias)+ self.x_screen_bias
+        rect[:, 1::2] = self.scale * (rect[:, 1::2] - self.y_index_bias) + self.y_screen_bias
+        return rect
+        
+    def change_index_bias(self, x, y):
+        self.x_index_bias += np.int((x - self.x_screen_bias) // self.scale)
+        self.y_index_bias += np.int((y - self.y_screen_bias) // self.scale)
     
     def get_mouse_index_coord(self, x, y):
-        m, n = np.shape(self.cell_field)
-        self.x_mouse = x
-        self.y_mouse = y
-        self.x_index_coord = round((x - self.x_screen_bias) // self.scale - self.x_index_bias)
-        self.y_index_coord = round((y - self.y_screen_bias) // self.scale - self.y_index_bias)
+        x_index = np.int((x - self.x_screen_bias) // self.scale + self.x_index_bias)
+        y_index = np.int((y - self.y_screen_bias) // self.scale + self.y_index_bias)
+        return x_index, y_index
         
     def add_cell(self, x, y):
-        self.get_mouse_index_coord(x, y)
-        if self.field_height > self.y_index_coord >= 0 and self.field_width > self.x_index_coord >= 0:
-            #self.cell_field[y_index_coord, x_index_coord] = not self.cell_field[y_index_coord, x_index_coord]
-            self.cell_field[self.y_index_coord, self.x_index_coord] = 1
+        j, i = self.get_mouse_index_coord(x, y)
+        if self.field_height > i >= 0 and self.field_width > j >= 0:
+            self.cell_field[i, j] = 1
 
         
 ###################
@@ -191,10 +170,10 @@ def draw(Rect, color, space):
         polygon(space, color, [r[0:2], r[2:4], r[4:6], r[6:8]])
         
 X, Y = 1000, 550
-start_FPS = 120
+start_FPS = 30
 max_FPS = 250
-Trun = 120
-Tshow = 60
+Trun = 30
+Tshow = 20
 x_start, y_start = 0, 0
 x_cur, y_cur = 0, 0
 track_mouse = 0
@@ -208,7 +187,7 @@ y_paint = 0
 play1, play2 = 0, 1
 
 
-regime = 1        ############
+regime = 3        ############
 
 
 def count_period(t, fps):
@@ -250,9 +229,10 @@ if __name__ == '__main__':
                     paint = 1
                 if event.button == 5:
                     x, y = pg.mouse.get_pos()
-                    game.get_mouse_index_coord(x, y)
-                    game.scale *= 0.8
-                    
+                    game.change_index_bias(x, y)
+                    game.x_screen_bias, game.y_screen_bias = x, y
+                    game.scale = np.round(game.scale * 0.9, 2)
+        
             elif event.type == pg.MOUSEBUTTONUP:
                 if event.button == 3:
                     track_mouse = 0
@@ -262,8 +242,9 @@ if __name__ == '__main__':
                     paint = 0
                 if event.button == 4:
                     x, y = pg.mouse.get_pos()
-                    game.get_mouse_index_coord(x, y)
-                    game.scale *= 1.2
+                    game.change_index_bias(x, y)
+                    game.x_screen_bias, game.y_screen_bias = x, y
+                    game.scale = np.round(game.scale * 1.1, 2)
                     
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
