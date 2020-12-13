82b9c8eaa92afb6502492f35e6cadf74515b3c2c
Oleg20502 <oleg20502@gmail.com>
2020-12-14 00:56:16 +0300

    Improved prog's structure
    Made game functions class

--- Game_of_life_1.py
+++ Game_of_life_1.py
@@ -9,42 +9,26 @@
 
 import numpy as np
 import pygame as pg
-import time as t
+#import time as t
 from pygame.draw import polygon
 from Format_transform import load_and_transform
-from Menu_of_game import Menu, Buttons
+from Menu_of_game import Menu
 
-WHITE = (255, 255, 255)
-RED = (225, 0, 50)
-BLACK = (0, 0, 0)
-lightBLACK = (3, 3, 3)
-lightGreen = (0, 255, 0)
-BLUE = (0, 0, 255, 20)
-GRAY = (125, 125, 125)
-lightBlue = (64, 128, 255)
-GREEN = (0, 200, 64, 20)
-YELLOW = (225, 225, 0, 2)
-PINK = (230, 50, 230, 0)
-ORANGE = (255, 165, 0)
-brown = (105, 82, 62, 255)
-darkorange3 = (205, 102, 0, 255)
-rosybrown5 = (159, 125, 125, 255)
-sandybrown = (244, 164, 96, 255)
-CLEAR = (0, 0, 0, 0)
-zapp = 'Запуск произвольного поля'
-zapz = 'Запуск произвольного загруженного из файла поля'
-zapn = 'Рисование рислвания своего поля'
-nas = 'Настройки'
-v = 'Выход'
-naz = 'Назад'
-yz = 'Язык'
-s = 'Звук'
-zast = 'Заставка'
-vcp = 'Выбор цвета пикселей'
-rus = 'Русский'
-eng = 'Английский'
 
-class Game_of_life():
+def life(cell_field):
+    #t1 = t.time()
+    # Подсчет соседей для каждой клетки кроме граничных
+    N = (cell_field[0:-2,0:-2] + cell_field[0:-2,1:-1] + cell_field[0:-2,2:] + cell_field[1:-1,0:-2] + 
+        cell_field[1:-1,2:] + cell_field[2: ,0:-2] + cell_field[2: ,1:-1] + cell_field[2: ,2:])
+    # Применение правил
+    birth = np.logical_and(N == 3, np.logical_not(cell_field[1:-1,1:-1]))
+    survive = np.logical_and(np.logical_or(N == 2, N == 3), cell_field[1:-1,1:-1])
+    cell_field[1:-1,1:-1] = np.logical_or(birth, survive)
+    #print('Новое поколение', t.time() - t1)
+    return cell_field
+
+
+class Game_functions():
     def __init__(self, screen_width, screen_height):
         self.screen_x = screen_width
         self.screen_y = screen_height
@@ -59,17 +43,6 @@ class Game_of_life():
         self.y_screen_bias = 0
         
         self.generation = 0
-        
-    def update_generation(self):
-        #t1 = t.time()
-        # Подсчет соседей для каждой клетки кроме граничных
-        N = (self.cell_field[0:-2,0:-2] + self.cell_field[0:-2,1:-1] + self.cell_field[0:-2,2:] + self.cell_field[1:-1,0:-2] + 
-            self.cell_field[1:-1,2:] + self.cell_field[2: ,0:-2] + self.cell_field[2: ,1:-1] + self.cell_field[2: ,2:])
-        # Применение правил
-        birth = np.logical_and(N == 3, np.logical_not(self.cell_field[1:-1,1:-1]))
-        survive = np.logical_and(np.logical_or(N == 2, N == 3), self.cell_field[1:-1,1:-1])
-        self.cell_field[1:-1,1:-1] = np.logical_or(birth, survive)
-        #print('Новое поколение', t.time() - t1)
         
     def broaden_field(self, border = 1):
         #t1 = t.time()
@@ -140,17 +113,8 @@ class Game_of_life():
         if run:
             self.generation += 1
             self.broaden_field()
-            self.update_generation()
+            self.cell_field = life(self.cell_field)
             self.shrink_field()
-    
-    def load_life(self):
-        Path = 'Patterns/Gosper_Gun.txt'  # FIXME
-        data_list = []
-        with open(Path, 'r') as f:
-            for line in f:
-                data_list.append(line.split(','))
-            self.cell_field = np.asarray(data_list, dtype='int')
-        self.broaden_field(0)
 
     def load(self):
         Path = 'Patterns/2c5-spaceship-gun-p416.rle'
@@ -162,7 +126,7 @@ class Game_of_life():
         
     def hanlde_bounds(self, x, axis = 0):
         if axis != 0 and axis != 1:
-            raise 'Axis should be 0 or 1'
+            raise TypeError ('Axis should be 0 or 1')
         upper_bound = (1 - axis) * self.field_width + axis * self.field_height
         x = max(x, 0)
         x = min(x, upper_bound)
@@ -198,49 +162,35 @@ class Game_of_life():
             self.cell_field[i, j] = 1
 
 
-###################
-#     VUSUALISATION
-###################
-
-
-
-
-
 def draw(Rect, color, space):
     # Рисуем квадратик,соответствующий каждой клетке
     for r in Rect:
         polygon(space, color, [r[0:2], r[2:4], r[4:6], r[6:8]])
 
-
-
-
-X, Y = 1000, 550
-start_FPS = 60
-max_FPS = 250
-Trun = 60
-Tshow = 20
-x_start, y_start = 0, 0
-x_cur, y_cur = 0, 0
-track_mouse = 0
-arrow_up_pressed = 0
-arrow_down_pressed = 0
-scroll_up = 0
-scroll_down = 0
-paint = 0
-x_paint = 0
-y_paint = 0
-play1, play2 = 0, 1
-
 def count_period(t, fps):
     return round(fps / t)
 
-if __name__ == '__main__':
-    game = Game_of_life(X, Y)
+def main():
+    X, Y = 1000, 550
+    start_FPS = 60
+    max_FPS = 250
+    Trun = 60
+    Tshow = 20
+    x_start, y_start = 0, 0
+    x_cur, y_cur = 0, 0
+    track_mouse = 0
+    scroll_up = 0
+    scroll_down = 0
+    paint = 0
+    x_paint = 0
+    y_paint = 0
+    play1, play2 = 0, 1
+
+    game = Game_functions(X, Y)
     
     pg.init()
     screen = pg.display.set_mode((X, Y))
     clock = pg.time.Clock()
-    screen.fill(WHITE)
     pg.display.set_caption('Conways_game_of_life')
     pg.mixer.music.load('sound in menu.ogg')
     pg.mixer.music.set_volume(0.35)
@@ -253,93 +203,92 @@ if __name__ == '__main__':
     clock = pg.time.Clock()
     FPS = start_FPS
     finished = False
-    menu = 1
-    col = RED
-    col_fon_game = WHITE
     M = Menu(X, Y, screen)
     M.main_menu()
-    BUT = Buttons(50, 50, screen)
-
-
-
-
 
     game.setup(M.menu)
-    print(menu)
-    if M.menu != 0:
-
-        t1, t2, t3 = 0, 0, 0
-        measure1 = 0
-        measure2 = 0
-        while not finished:
-            t1 += 1
-            period_run = count_period(Trun, FPS)
-            period_show = count_period(Tshow, FPS)
-            clock.tick(FPS)
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    finished = True
-                    
-                elif event.type == pg.MOUSEBUTTONDOWN:
-                    if event.button == 3:
-                        track_mouse = 1
-                        x_start, y_start = pg.mouse.get_pos()
-                    if event.button  == 1:
-                        play2 = 0
-                        FPS = max_FPS
-                        paint = 1
-                    if event.button == 5:
-                        x, y = pg.mouse.get_pos()
-                        game.change_index_bias(x, y)
-                        game.x_screen_bias, game.y_screen_bias = x, y
-                        game.scale = np.round(max(game.scale * 0.9, 0.5), 2)
-            
-                elif event.type == pg.MOUSEBUTTONUP:
-                    if event.button == 3:
-                        track_mouse = 0
-                    if event.button == 1:
-                        play2 = 1
-                        FPS = start_FPS
-                        paint = 0
-                    if event.button == 4:
-                        x, y = pg.mouse.get_pos()
-                        game.change_index_bias(x, y)
-                        game.x_screen_bias, game.y_screen_bias = x, y
-                        game.scale = np.round(min(game.scale * 1.1, 50), 2)
-                        
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_SPACE:
-                        play1 = not play1
-            
-            if track_mouse == 1:
-                x_cur, y_cur = pg.mouse.get_pos()
-                game.x_screen_bias += x_cur - x_start
-                game.y_screen_bias += y_cur - y_start
-                x_start, y_start = x_cur, y_cur
-            if paint and (not play1 or not play2):
-                game.adjust_field()
-                x_paint , y_paint = pg.mouse.get_pos()
-                game.add_cell(x_paint, y_paint)
-                
-            if t1 % period_run == 0:  
-                time1 = t.time()
-                game.run(play1 and play2)
-                time2 = t.time()
-                t2 += 1
-                measure1 += time2 - time1
-                #print('Поколение:', game.generation)
-            if t1 % period_show == 0:
-                time3 = t.time()
-                draw(game.rect_coordinetes(), M.col, screen)
-                pg.display.update()
-                screen.fill(M.col_fon_game)
-                time4 = t.time()
-                t3 += 1
-                measure2 += time4 - time3
     
+    counter = 0
+    #t2, t3 = 0, 0
+    #measure1 = 0
+    #measure2 = 0
+    while not finished:
+        counter += 1
+        period_run = count_period(Trun, FPS)
+        period_show = count_period(Tshow, FPS)
+        clock.tick(FPS)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                finished = True
+                
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 3:
+                    track_mouse = 1
+                    x_start, y_start = pg.mouse.get_pos()
+                if event.button  == 1:
+                    play2 = 0
+                    FPS = max_FPS
+                    paint = 1
+                if event.button == 5:
+                    scroll_down = 1
+        
+            elif event.type == pg.MOUSEBUTTONUP:
+                if event.button == 3:
+                    track_mouse = 0
+                if event.button == 1:
+                    play2 = 1
+                    FPS = start_FPS
+                    paint = 0
+                if event.button == 4:
+                    scroll_up = 1
+                    
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    play1 = not play1
+        
+        if scroll_down:
+            x, y = pg.mouse.get_pos()
+            game.change_index_bias(x, y)
+            game.x_screen_bias, game.y_screen_bias = x, y
+            game.scale = np.round(max(game.scale * 0.9, 0.5), 2)
+            scroll_down = 0
+            
+        if scroll_up:
+            x, y = pg.mouse.get_pos()
+            game.change_index_bias(x, y)
+            game.x_screen_bias, game.y_screen_bias = x, y
+            game.scale = np.round(min(game.scale * 1.1, 50), 2)
+            scroll_up = 0
+        
+        if track_mouse == 1:
+            x_cur, y_cur = pg.mouse.get_pos()
+            game.x_screen_bias += x_cur - x_start
+            game.y_screen_bias += y_cur - y_start
+            x_start, y_start = x_cur, y_cur
+        if paint and (not play1 or not play2):
+            game.adjust_field()
+            x_paint , y_paint = pg.mouse.get_pos()
+            game.add_cell(x_paint, y_paint)
+            
+        if counter % period_run == 0:  
+            #time1 = t.time()
+            game.run(play1 and play2)
+            #time2 = t.time()
+            #t2 += 1
+            #measure1 += time2 - time1
+            #print('Поколение:', game.generation)
+        if counter % period_show == 0:
+            #time3 = t.time()
+            draw(game.rect_coordinetes(), M.col, screen)
+            pg.display.update()
+            screen.fill(M.col_fon_game)
+            #time4 = t.time()
+            #t3 += 1
+            #measure2 += time4 - time3
     pg.quit()
-    print('Жизнь', round(measure1/t2, 4))
-    print('Pygame', round(measure2/t3, 4))
+    #print('Жизнь', round(measure1/t2, 4))
+    #print('Pygame', round(measure2/t3, 4))
 
-
+if __name__ == '__main__':
+    main()
 
